080b9c242f4d93206a9342058755d8bb07735759
Ludovic Chabant <ludovic@chabant.com>
2014-08-18 16:54:00 -0700

    Add support for Markdown extensions.

--- piecrust/formatting/markdownformatter.py
+++ piecrust/formatting/markdownformatter.py
@@ -6,6 +6,23 @@ class MarkdownFormatter(Formatter):
     FORMAT_NAMES = ['markdown', 'mdown', 'md']
     OUTPUT_FORMAT = 'html'
 
+    def __init__(self):
+        super(MarkdownFormatter, self).__init__()
+        self._extensions = None
+
     def render(self, format_name, txt):
-        return markdown(txt)
+        self._ensureInitialized()
+        return markdown(txt, extensions=self._extensions)
+
+    def _ensureInitialized(self):
+        if self._extensions is not None:
+            return
+
+        extensions = self.app.config.get('markdown/extensions')
+        if extensions is None:
+            extensions = []
+        # Compatibility with PieCrust 1.x
+        if self.app.config.get('markdown/use_markdown_extra'):
+            extensions.append('extra')
+        self._extensions = extensions
 
