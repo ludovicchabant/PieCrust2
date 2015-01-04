e5301808c4ec64502d3bc125286597ea9cf71937
Ludovic Chabant <ludovic@chabant.com>
2015-01-04 14:53:13 -0800

    tests: Add help functions to get and render a simple page.

--- tests/mockutil.py
+++ tests/mockutil.py
@@ -4,10 +4,11 @@ import random
 import codecs
 import shutil
 import os.path
-import functools
 import mock
 import yaml
 from piecrust.app import PieCrust, PieCrustConfiguration
+from piecrust.page import Page
+from piecrust.rendering import PageRenderingContext, render_page
 
 
 resources_path = os.path.abspath(
@@ -20,6 +21,18 @@ def get_mock_app(config=None):
     app = mock.MagicMock(spec=PieCrust)
     app.config = PieCrustConfiguration()
     return app
+
+
+def get_simple_page(app, rel_path):
+    source = app.getSource('pages')
+    metadata = {'path': os.path.splitext(rel_path)[0]}
+    return Page(source, metadata, rel_path)
+
+
+def render_simple_page(page, uri):
+    ctx = PageRenderingContext(page, uri)
+    rp = render_page(ctx)
+    return rp.content
 
 
 class _MockFsEntry(object):
