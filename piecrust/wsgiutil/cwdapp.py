8ba3e26143f31a98869cf7c3889993df1c75842d
Ludovic Chabant <ludovic@chabant.com>
2015-05-07 22:47:44 -0700

    serve: Add a WSGI utility module for easily getting a default app.
    This makes it easy to run Gunicorn from the command-line using the Gunicorn
    script instead of the `chef serve` command.

---
+++ piecrust/wsgiutil/cwdapp.py
@@ -0,0 +1,11 @@
+# This is a utility module that can be used with any WSGI-compatible server
+# like Werkzeug or Gunicorn. It returns a WSGI app for serving a PieCrust
+# website located in the current working directory.
+import os
+from piecrust.serving.server import Server
+
+
+root_dir = os.getcwd()
+server = Server(root_dir, sub_cache_dir='prod', enable_debug_info=False)
+app = server.getWsgiApp()
+
