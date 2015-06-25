577aa82e01bc2126455d0326faf5e69907d5b18b
Ludovic Chabant <ludovic@chabant.com>
2015-06-25 08:31:24 -0700

    render: Lazily import Textile package.

--- piecrust/formatting/textileformatter.py
+++ piecrust/formatting/textileformatter.py
@@ -1,4 +1,3 @@
-from textile import textile
 from piecrust.formatting.base import Formatter
 
 
@@ -7,6 +6,7 @@ class TextileFormatter(Formatter):
     OUTPUT_FORMAT = 'html'
 
     def render(self, format_name, text):
+        from textile import textile
         assert format_name in self.FORMAT_NAMES
         return textile(text)
 
