e86f259b97833be8d164f348249a84c8410260b4
Ludovic Chabant <ludovic@chabant.com>
2015-07-28 21:50:57 -0700

    bug: Fix file-system wrappers for non-Mac systems.

--- piecrust/osutil.py
+++ piecrust/osutil.py
@@ -1,23 +1,28 @@
 import os
 import sys
-import glob as _glob
+import glob as _system_glob
 import unicodedata
 
 
+walk = os.walk
+listdir = os.listdir
+glob = _system_glob.glob
+
+
 if sys.platform == 'darwin':
-    def walk(top, **kwargs):
+    def _walk(top, **kwargs):
         for dirpath, dirnames, filenames in os.walk(top, **kwargs):
             dirpath = _from_osx_fs(dirpath)
             dirnames = list(map(_from_osx_fs, dirnames))
             filenames = list(map(_from_osx_fs, filenames))
             yield dirpath, dirnames, filenames
 
-    def listdir(path='.'):
+    def _listdir(path='.'):
         for name in os.listdir(path):
             name = _from_osx_fs(name)
             yield name
 
-    def glob(pathname):
+    def _glob(pathname):
         pathname = _to_osx_fs(pathname)
         matches = _glob.glob(pathname)
         return list(map(_from_osx_fs, matches))
@@ -28,8 +33,7 @@ if sys.platform == 'darwin':
     def _to_osx_fs(s):
         return unicodedata.ucd_3_2_0.normalize('NFD', s)
 
-else:
-    walk = sys.walk
-    listdir = sys.listdir
-    glob = _glob.glob
+    walk = _walk
+    listdir = _listdir
+    glob = _glob
 
