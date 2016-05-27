c7d79ddda6a89112ded4767ac571ac7a4ddaedb6
Ludovic Chabant <ludovic@chabant.com>
2016-05-26 19:46:28 -0700

    internal: Remove unused piece of code.

--- piecrust/uriutil.py
+++ piecrust/uriutil.py
@@ -2,70 +2,9 @@ import re
 import os.path
 import string
 import logging
-import functools
 
 
 logger = logging.getLogger(__name__)
-
-
-class UriError(Exception):
-    def __init__(self, uri):
-        super(UriError, self).__init__("Invalid URI: %s" % uri)
-
-
-@functools.total_ordering
-class UriInfo(object):
-    def __init__(self, uri, source, args, taxonomy=None, page_num=1):
-        self.uri = uri
-        self.source = source
-        self.args = args
-        self.taxonomy = taxonomy
-        self.page_num = page_num
-
-    def __eq__(self, other):
-        return ((self.uri, self.source, self.args, self.taxonomy,
-                self.page_num) ==
-            (other.uri, other.source, other.args, other.taxonomy,
-                other.page_num))
-
-    def __lt__(self, other):
-        return ((self.uri, self.source, self.args, self.taxonomy,
-                self.page_num) <
-            (other.uri, other.source, other.args, other.taxonomy,
-                other.page_num))
-
-
-pagenum_pattern = re.compile(r'/(\d+)/?$')
-
-
-def parse_uri(routes, uri):
-    if uri.find('..') >= 0:
-        raise UriError(uri)
-
-    page_num = 1
-    match = pagenum_pattern.search(uri)
-    if match is not None:
-        uri = uri[:match.start()]
-        page_num = int(match.group(1))
-
-    uri = '/' + uri.strip('/')
-
-    for rn, rc in routes.items():
-        pattern = route_to_pattern(rn)
-        m = re.match(pattern, uri)
-        if m is not None:
-            args = m.groupdict()
-            return UriInfo(uri, rc['source'], args, rc.get('taxonomy'),
-                    page_num)
-
-    return None
-
-
-r2p_pattern = re.compile(r'%(\w+)%')
-
-
-def route_to_pattern(route):
-    return r2p_pattern.sub(r'(?P<\1>[\w\-]+)', route)
 
 
 def multi_replace(text, replacements):
--- tests/test_uriutil.py
+++ tests/test_uriutil.py
@@ -1,29 +1,6 @@
 import mock
 import pytest
-from piecrust.uriutil import UriInfo, parse_uri, split_sub_uri
-
-
-@pytest.mark.parametrize('routes, uri, expected', [
-        ({}, '/foo', None),
-        (
-            {'/articles/%slug%': {'source': 'dummy'}},
-            '/articles/foo',
-            UriInfo('', 'dummy', {'slug': 'foo'})),
-        (
-            {'/foo/%bar%': {'source': 'foo'},
-                '/other/%one%-%two%': {'source': 'other'}},
-            '/other/some-thing',
-            UriInfo('', 'other', {'one': 'some', 'two': 'thing'}))
-    ])
-def test_parse_uri(routes, uri, expected):
-    if expected is not None:
-        expected.uri = uri
-    for pattern, args in routes.items():
-        if 'taxonomy' not in args:
-            args['taxonomy'] = None
-
-    actual = parse_uri(routes, uri)
-    assert actual == expected
+from piecrust.uriutil import split_sub_uri
 
 
 @pytest.mark.parametrize('uri, expected, pretty_urls', [
