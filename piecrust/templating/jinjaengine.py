1de8fc186a39aa386cac19acf7f4780cc0346562
Ludovic Chabant <ludovic@chabant.com>
2017-06-08 23:31:45 -0700

    debug: Raise Jinja template exceptions the same way in both render passes.

--- piecrust/templating/jinjaengine.py
+++ piecrust/templating/jinjaengine.py
@@ -72,6 +72,8 @@ class JinjaTemplateEngine(TemplateEngine):
         except AbortedSourceUseError:
             raise
         except Exception as ex:
+            if self.app.debug:
+                raise
             msg = "Error rendering Jinja markup"
             rel_path = os.path.relpath(rendered_path, self.app.root_dir)
             raise TemplatingError(msg, rel_path) from ex
