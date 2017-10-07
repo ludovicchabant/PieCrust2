d79e1eadff283bf20c13a03579d0a589fa501bd7
Ludovic Chabant <ludovic@chabant.com>
2017-10-07 12:30:49 -0700

    admin: Fix the site preview by editing the WSGI environment.

--- piecrust/admin/views/preview.py
+++ piecrust/admin/views/preview.py
@@ -1,4 +1,4 @@
-from flask import g, make_response
+from flask import g, request, make_response
 from flask.ext.login import login_required
 from piecrust.serving.server import PieCrustServer
 from ..blueprint import foodtruck_bp
@@ -15,6 +15,16 @@ def preview_root_page():
 def preview_page(url):
     site = g.site
     pcappfac = site.piecrust_factory
-    server = PieCrustServer(pcappfac, root_url=site.make_url('/preview/'))
+    root_url = request.script_root or ''
+    root_url += site.make_url('/preview/')
+    server = PieCrustServer(pcappfac, root_url=root_url)
+
+    # Patch the WSGI environment for the underlying PieCrust server,
+    # because it doesn't generally handle stuff being under a different
+    # sub folder of the domain.
+    script_name = request.environ['SCRIPT_NAME']
+    request.environ['SCRIPT_NAME'] = ''
+    request.environ['PATH_INFO'] = script_name + request.environ['PATH_INFO']
+
     return make_response(server)
 
