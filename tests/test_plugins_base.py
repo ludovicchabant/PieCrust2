74b48dd82ccafdd14f6cdb5b0f822307e1bc2568
Ludovic Chabant <ludovic@chabant.com>
2017-12-27 13:25:53 -0800

    tests: Add plugin tests.

---
+++ tests/test_plugins_base.py
@@ -0,0 +1,29 @@
+from .mockutil import mock_fs, mock_fs_scope
+
+
+def test_no_plugins():
+    fs = (mock_fs()
+          .withConfig())
+    with mock_fs_scope(fs):
+        app = fs.getApp()
+        assert len(app.plugin_loader.plugins) == 1
+        assert app.plugin_loader.plugins[0].name == '__builtin__'
+
+
+testplug_code = """from piecrust.plugins.base import PieCrustPlugin
+
+class TestPlugPlugin(PieCrustPlugin):
+    name = 'just a test plugin'
+
+__piecrust_plugin__ = TestPlugPlugin
+"""
+
+def test_loose_file():
+    fs = (mock_fs()
+          .withConfig({'site': {'plugins': 'testplug'}})
+          .withFile('kitchen/plugins/testplug.py', testplug_code))
+    with mock_fs_scope(fs):
+        app = fs.getApp()
+        assert sorted([p.name for p in app.plugin_loader.plugins]) == \
+          sorted(['__builtin__', 'just a test plugin'])
+        assert False
