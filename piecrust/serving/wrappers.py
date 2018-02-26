25971b4ce6e38ff021b1e6e9ca2a85baeef49655
Ludovic Chabant <ludovic@chabant.com>
2018-02-25 23:18:47 -0800

    serve: Indicate where to find the admin panel in the console output.

--- piecrust/serving/wrappers.py
+++ piecrust/serving/wrappers.py
@@ -100,6 +100,11 @@ def _run_werkzeug_server(appfactory, host, port, *,
     # Disable debugger PIN protection.
     os.environ['WERKZEUG_DEBUG_PIN'] = 'off'
 
+    if is_cmdline_mode:
+        admin_url = 'http://%s:%s%s' % (host, port, '/pc-admin')
+        logger.info("The administrative panel is available at: %s" %
+                    admin_url)
+
     try:
         run_simple(host, port, app,
                    threaded=True,
