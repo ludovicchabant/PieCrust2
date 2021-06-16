16ccbdfbe3b2247fe7e5fd272e674195a7872e9c
Ludovic Chabant <ludovic@chabant.com>
2021-06-16 09:28:42 -0700

    cm: Update node packages.

--- gulpfile.js
+++ gulpfile.js
@@ -29,13 +29,14 @@ gulp.task('sass', function() {
 gulp.task('js', function() {
     return gulp.src([
             'node_modules/jquery/dist/jquery.min.js',
-            'node_modules/bootstrap/dist/js/bootstrap.min.js',
             'node_modules/timeago/jquery.timeago.js',
+            'node_modules/popper.js/dist/umd/popper.min.js',
+            'node_modules/bootstrap/dist/js/bootstrap.min.js',
             'piecrust/admin/assets/js/**/*.js'
             ])
         .pipe(sourcemaps.init())
         .pipe(concat('foodtruck.js'))
-        //.pipe(uglify())
+        .pipe(uglify())
         .pipe(sourcemaps.write())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('piecrust/admin/static/js'));
